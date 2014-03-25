f0619bb6d57c3e9a7047f4cc16190c310d609946
jfarcand <jfarcand@apache.org>
2014-03-25 17:34:12 -0400

    Fixes #89

--- modules/javascript/src/main/webapp/javascript/atmosphere.js
+++ modules/javascript/src/main/webapp/javascript/atmosphere.js
@@ -1349,7 +1349,7 @@
                     } else if (!webSocketOpened) {
                         _reconnectWithFallbackTransport("Websocket failed. Downgrading to Comet and resending");
 
-                    } else if (_request.reconnect && _response.transport === 'websocket') {
+                    } else if (_request.reconnect && _response.transport === 'websocket' && message.code !== 1001) {
                         _clearState();
                         if (_requestCount++ < _request.maxReconnectOnClose) {
                             _open('re-connecting', _request.transport, _request);
