68de196289ed904a165d84ed7e3bba4677114405
jfarcand <jfarcand@apache.org>
2014-05-08 14:38:13 -0400

    proper fix

--- modules/jquery/src/main/webapp/jquery/jquery.atmosphere.js
+++ modules/jquery/src/main/webapp/jquery/jquery.atmosphere.js
@@ -2799,7 +2799,7 @@
             }
 
             // https://github.com/Atmosphere/atmosphere-javascript/issues/58
-            uuid = ((typeof (request) !== 'undefined') && typeof (request.uuid) !== 'undefined') ? request.uuid : 0;
+            jQuery.atmosphere.uuid = ((typeof (request) !== 'undefined') && typeof (request.uuid) !== 'undefined') ? request.uuid : 0;
 
             if (typeof (url) !== "string") {
                 request = url;
