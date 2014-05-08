71b8e10ffd96fb4f5da36eab74a3e33eb56e16b9
jfarcand <jfarcand@apache.org>
2014-05-08 13:49:35 -0400

    Fix cut&paste

--- modules/jquery/src/main/webapp/jquery/jquery.atmosphere.js
+++ modules/jquery/src/main/webapp/jquery/jquery.atmosphere.js
@@ -1405,7 +1405,7 @@
                             }
                         }
                     }
-                } else if (request.enableProtocol && request.firstMessage && atmosphere.util.browser.msie && +atmosphere.util.browser.version.split(".")[0] < 10) {
+                } else if (request.enableProtocol && request.firstMessage && jQuery.browser.msie && +jQuery.browser.version.split(".")[0] < 10) {
                     // In case we are getting some junk from IE
                 } else {
                     _triggerOpen(request);
