b5918a6dc95dc0ae0c3468f6c1e5c1eadaa4b2f9
Oskar Norrback <oskar@teonos.com>
2015-02-17 17:43:53 +0200

    Clear previous reconnect timer if one already exists
    This seems to mitigate a race condition during reconnect that can cause
    the client to go into a reconnect-disconnect loop.

--- modules/javascript/src/main/webapp/javascript/atmosphere.js
+++ modules/javascript/src/main/webapp/javascript/atmosphere.js
@@ -1519,6 +1519,10 @@
                         if (_requestCount++ < _request.maxReconnectOnClose) {
                             _open('re-connecting', _request.transport, _request);
                             if (_request.reconnectInterval > 0) {
+                                if(_request.reconnectId) {
+                                    clearTimeout(_request.reconnectId);
+                                    delete _request.reconnectId;
+                                }
                                 _request.reconnectId = setTimeout(function () {
                                     _response.responseBody = "";
                                     _response.messages = [];
