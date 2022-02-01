d4a65d0946faf089a3c79f1d57894ced8e8a3b01
jfarcand <jfarcand@apache.org>
2022-02-01 16:43:34 -0500

    Update copyright

--- modules/javascript/src/main/webapp/javascript/atmosphere.js
+++ modules/javascript/src/main/webapp/javascript/atmosphere.js
@@ -1,5 +1,5 @@
 /*
- * Copyright 2011-2021 Async-IO.org
+ * Copyright 2011-2022 Async-IO.org
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
